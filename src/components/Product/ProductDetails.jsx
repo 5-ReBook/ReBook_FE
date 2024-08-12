e7a790607dd04868cb61ad5d87797c98af65d1bc
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-08-12 15:47:20 +0900

    [feat] 상품 상세정보(사용대학, 전공) 컴포넌트 생성

---
+++ src/components/Product/ProductDetails.jsx
@@ -0,0 +1,14 @@
+const ProductDetails = ({ university, major }) => {
+  return (
+    <div className="product-details">
+      <p>
+        <strong>사용 대학 :</strong> {university}
+      </p>
+      <p>
+        <strong>전공 :</strong> {major}
+      </p>
+    </div>
+  );
+};
+
+export default ProductDetails;
