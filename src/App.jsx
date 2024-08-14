10a66a75d57ae22d7278a06440a0ecb82ff138c3
kimjaehyuk <jhkim0097@gmail.com>
2024-08-14 14:59:41 +0900

    conflict 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import ProductRegistrationPage from "./pages/ProductRegistrationPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
-import MyProductsPage from "./pages/MyProductsPage";
+// import ProductRegistrationPage from "./pages/ProductRegistrationPage";
+// import ProductDetailPage from "./pages/ProductDetailPage";
+// import MyProductsPage from "./pages/MyProductsPage";
 
 function App() {
   const isAuthenticated = localStorage.getItem("Authorization");
 
   return (
-    <>
-      <Routes>
-        <Route
-          path="/signin"
-          element={!isAuthenticated ? <Signin /> : <Navigate to="/product" />}
-        />
-        <Route
-          path="/product"
-          element={isAuthenticated ? <Product /> : <Navigate to="/signin" />}
-        />
-        <Route
-          path="/"
-          element={<Navigate to={isAuthenticated ? "/product" : "/signin"} />}
-        />
-        <Route path="/" element={<MainPage />} />
-        <Route path="/products/new" element={<ProductRegistrationPage />} />
-        <Route path="/products/detail" element={<ProductDetailPage />} />
-        <Route path="/products/me" element={<MyProductsPage />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      {/* <Route
+        path="/signin"
+        element={!isAuthenticated ? <Signin /> : <Navigate to="/product" />}
+      /> */}
+      {/* <Route
+        path="/product"
+        element={isAuthenticated ? <Product /> : <Navigate to="/signin" />}
+      /> */}
+      {/* <Route path="/products/new" element={<ProductRegistrationPage />} /> */}
+      {/* <Route path="/products/detail" element={<ProductDetailPage />} /> */}
+      {/* <Route path="/products/me" element={<MyProductsPage />} /> */}
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <h1>REBOOK!!!!</h1>
     <App />
   </BrowserRouter>
 );
