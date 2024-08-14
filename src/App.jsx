8f710d60f2e95d706c627ddc5d04f8847c09436c
Austin-Choi <austinsupra@gmail.com>
2024-08-14 15:37:54 +0900

    fix : 로그인 하면 상품 목록 보이고 안하면 로그인 페이지 보이게  수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-// import ProductRegistrationPage from "./pages/ProductRegistrationPage";
-// import ProductDetailPage from "./pages/ProductDetailPage";
-// import MyProductsPage from "./pages/MyProductsPage";
+import Signin from "./pages/Signin/Signin";
+import ProductRegistrationPage from "./pages/ProductRegistrationPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import MyProductsPage from "./pages/ProductDetailPage";
+
 
 function App() {
   const isAuthenticated = localStorage.getItem("Authorization");
 
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      {/* <Route
+      <Route
         path="/signin"
-        element={!isAuthenticated ? <Signin /> : <Navigate to="/product" />}
-      /> */}
-      {/* <Route
-        path="/product"
-        element={isAuthenticated ? <Product /> : <Navigate to="/signin" />}
-      /> */}
-      {/* <Route path="/products/new" element={<ProductRegistrationPage />} /> */}
-      {/* <Route path="/products/detail" element={<ProductDetailPage />} /> */}
-      {/* <Route path="/products/me" element={<MyProductsPage />} /> */}
+        element={!isAuthenticated ? <Signin /> : <Navigate to="/products"/>}
+      />
+      <Route
+        path="/"
+        element={isAuthenticated ? <MainPage/> : <Navigate to="/signin"/>}
+      />
+      <Route
+        path="/products/new"
+        element={<ProductRegistrationPage />}
+      />
+      <Route
+        path="/products/detail"
+        element={<ProductDetailPage />}
+      />
+      <Route
+        path="/products/me"
+        element={<MyProductsPage />}
+      />
     </Routes>
   );
 }
