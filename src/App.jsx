b47bc8eaf40ae8a7bc646fe7e652702115069d6e
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-08-16 18:05:31 +0900

    [feat] 공통 레이아웃 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,33 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import MainPage from "./pages/MainPage";
-import Signin from "./pages/Signin/Signin";
-import ProductRegistrationPage from "./pages/ProductRegistrationPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
-import MyProductsPage from "./pages/ProductDetailPage";
-import SignupForm from "./pages/Signup/SignupForm";
-
+import { Routes, Route, Navigate } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import Signin from './pages/Signin/Signin';
+import ProductRegistrationPage from './pages/ProductRegistrationPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import MyProductsPage from './pages/ProductDetailPage';
+import SignupForm from './pages/Signup/SignupForm';
+import './App.css';
 
 function App() {
-  const isAuthenticated = localStorage.getItem("Authorization");
+  const isAuthenticated = localStorage.getItem('Authorization');
 
   return (
-    <Routes>
-      <Route
-        path="/signin"
-        element={!isAuthenticated ? <Signin /> : <Navigate to="/products"/>}
-      />
-      <Route
-        path="/"
-        element={isAuthenticated ? <MainPage/> : <Navigate to="/signin"/>}
-      />
-      <Route
-        path="/signupform"
-        element={<SignupForm/>}
-      />
-      <Route
-        path="/products/new"
-        element={<ProductRegistrationPage />}
-      />
-      <Route
-        path="/products/detail"
-        element={<ProductDetailPage />}
-      />
-      <Route
-        path="/products/me"
-        element={<MyProductsPage />}
-      />
-    </Routes>
+    <div className="container">
+      <Routes>
+        <Route
+          path="/signin"
+          element={!isAuthenticated ? <Signin /> : <Navigate to="/products" />}
+        />
+        <Route
+          path="/"
+          element={isAuthenticated ? <MainPage /> : <Navigate to="/signin" />}
+        />
+
+        <Route path="/signupform" element={<SignupForm />} />
+        <Route path="/products/new" element={<ProductRegistrationPage />} />
+        <Route path="/products/detail" element={<ProductDetailPage />} />
+        <Route path="/products/me" element={<MyProductsPage />} />
+      </Routes>
+    </div>
   );
 }
 
