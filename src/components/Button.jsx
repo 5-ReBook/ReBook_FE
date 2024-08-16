00e297578e39c2dbb4b745cf7a6d264fda34d8c5
Austin-Choi <austinsupra@gmail.com>
2024-08-16 10:22:43 +0900

    patch : 공통 Button 컴포넌트 사용하는 쪽에서 style override 가능하게 파라미터 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import './Button.css';
 
-const Button = ({ text, onClick }) => {
+// 사용하는 쪽에서 css override 가능하게 수정
+const Button = ({ text, onClick, className = "" }) => {
   return (
-    <button onClick={onClick} className="Button">
+    <button onClick={onClick} className={`Button ${className}`}>
       {text}
     </button>
   );
