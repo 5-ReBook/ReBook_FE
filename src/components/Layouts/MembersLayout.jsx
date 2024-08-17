caa6a1561522f45c093f2c512496daadfa95beae
Austin-Choi <austinsupra@gmail.com>
2024-08-17 18:04:16 +0900

    patch : 버튼 이미지로 바꿔서 깔끔한 느낌 강조

--- src/components/Layouts/MembersLayout.jsx
+++ src/components/Layouts/MembersLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../Header';
+import './MembersLayout.css';
 
 function MembersLayout({ children }) {
   const nav = useNavigate();
@@ -11,7 +12,7 @@ function MembersLayout({ children }) {
         title="ReBook"
         leftChild={
           <button type="button" onClick={() => nav(-1)}>
-            {'<'}
+            <img src="src/assets/images/left-chevron.png" alt="버튼" />
           </button>
         }
       />
