ce27eae309cdc4b9f0390ac2800955a141701a7e
Austin-Choi <austinsupra@gmail.com>
2024-08-17 16:22:57 +0900

    fix : signin 작동하게 수정

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -12,22 +12,29 @@ function Signin() {
 
   const handleSignin = async () => {
     try {
-      const response = await axios.post('https://localhost/auth/signin', {
-        username,
-        password,
-      });
+      const response = await axios.post(
+        'http://localhost/auth/signin',
+        {
+          username,
+          password,
+        },
+        {
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
 
       // 서버로부터 받은 액세스 토큰을 localStorage에 저장
       const accessToken = response.headers.access;
       localStorage.setItem('Authorization', accessToken);
 
-      // 로그인 성공 후 필요한 로직 처리 (예: 페이지 이동)
+      // 로그인 성공 후 필요한 로직 처리
       console.log('로그인 성공:', accessToken);
-
-      // 페이지 이동 또는 상태 업데이트 등
+      nav('/');
     } catch (error) {
       console.error('로그인 실패:', error);
-      // 에러 처리 (예시: 경고창, 메시지 표시 등)
     }
   };
 
