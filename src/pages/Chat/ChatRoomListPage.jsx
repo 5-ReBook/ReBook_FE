696c0c278c1a76ffbb75c6d1dc3e51720dc64e49
kimjaehyuk <jhkim0097@gmail.com>
2024-08-19 13:50:03 +0900

    [feat] 예시 chat Page 라우트 추가 #17

---
+++ src/pages/Chat/ChatRoomListPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ChatRoomListPage() {
+
+    return (
+        <div>
+            <h1>Chat Room List</h1>
+            <p>Chat Room List Page</p>
+        </div>
+    )
+}
+
+export default ChatRoomListPage;
