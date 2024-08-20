c5ae0ab4c699e396ac419d4192651a6431bbd0c3
kimjaehyuk <jhkim0097@gmail.com>
2024-08-20 18:35:11 +0900

    merge develop

--- src/App.jsx
+++ src/App.jsx
@@ -10,49 +10,51 @@ import MyProductsPage from './pages/MyProductsPage';
 import SignupForm from './pages/Signup/SignupForm';
 import FindPassword from './pages/FindPassword/FindPassword';
 import './App.css';
+import ChatRoomListPage from './pages/Chat/ChatRoomListPage';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const hasAccess = localStorage.getItem('Authorization');
+  // useEffect(() => {
+  //   const hasAccess = localStorage.getItem('Authorization');
 
-    if (hasAccess) {
-      setIsAuthenticated(true);
-      setIsLoading(false);
-    } else {
-      // 리프레시 토큰이 있다면 서버에 새 액세스 토큰 발급 요청
-      axios
-        .post(
-          'http://localhost/auth/members/refreshtoken/reissue',
-          {},
-          {
-            withCredentials: true, // HttpOnly 쿠키를 포함해 요청을 보냄
-          }
-        )
-        .then(response => {
-          const newAccessToken = response.headers.access;
-          if (newAccessToken) {
-            localStorage.setItem('Authorization', newAccessToken);
-            setIsAuthenticated(true);
-          } else {
-            throw new Error('액세스 토큰 획득에 실패했습니다.');
-          }
-        })
-        .catch(() => {
-          localStorage.removeItem('Authorization');
-          setIsAuthenticated(false);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    }
-  }, []);
+  //   if (hasAccess) {
+  //     setIsAuthenticated(true);
+  //     setIsLoading(false);
+  //   } else {
+  //     // 리프레시 토큰이 있다면 서버에 새 액세스 토큰 발급 요청
+  //     axios
+  //       .post(
+  //         'https://localhost/auth/members/refreshtoken/reissue',
+  //         {},
+  //         {
+  //           withCredentials: true, // HttpOnly 쿠키를 포함해 요청을 보냄
+  //         }
+  //       )
+  //       .then(response => {
+  //         const newAccessToken = response.headers.access;
+  //         if (newAccessToken) {
+  //           localStorage.setItem('Authorization', newAccessToken);
+  //           setIsAuthenticated(true);
+  //         } else {
+  //           throw new Error('액세스 토큰 획득에 실패했습니다.');
+  //         }
+  //       })
+  //       .catch(() => {
+  //         localStorage.removeItem('Authorization');
+  //         setIsAuthenticated(false);
+  //       })
+  //       .finally(() => {
+  //         setIsLoading(false);
+  //       });
+  //   }
+  // }, []);
 
-  if (isLoading) {
-    return <div>Loading...</div>; // 로딩 중 표시될 화면
-  }
+  // if (isLoading) {
+  //   return <div>Loading...</div>; // 로딩 중 표시될 화면
+  // }
 
   return (
     <Layout>
@@ -66,29 +68,11 @@ function App() {
           element={isAuthenticated ? <MainPage /> : <Navigate to="/signin" />}
         />
         <Route path="/signupform" element={<SignupForm />} />
-        <Route
-          path="/products/new"
-          element={
-            isAuthenticated ? (
-              <ProductRegistrationPage />
-            ) : (
-              <Navigate to="/signin" />
-            )
-          }
-        />
-        <Route
-          path="/products/detail"
-          element={
-            isAuthenticated ? <ProductDetailPage /> : <Navigate to="/signin" />
-          }
-        />
-        <Route
-          path="/products/me"
-          element={
-            isAuthenticated ? <MyProductsPage /> : <Navigate to="/signin" />
-          }
-        />
-        <Route path="/findpassword" element={<FindPassword />} />
+        <Route path="/products/new" element={<ProductRegistrationPage />} />
+        <Route path="/products/detail" element={<ProductDetailPage />} />
+        <Route path="/products/me" element={<MyProductsPage />} />
+        <Route path="/chat/roomlist" element={<ChatRoomListPage />} />
+        {/* <Route path="*" element={<Navigate to="/"></Navigate>} */}
       </Routes>
     </Layout>
   );
--- src/pages/Chat/ChatRoomListPage.jsx
+++ src/pages/Chat/ChatRoomListPage.jsx
@@ -1,13 +1,79 @@
 import React from 'react';
+import './ChatRoomListPage.css';
 
-function ChatRoomListPage() {
+const chats = [
+  {
+    id: 1,
+    name: '이서준',
+    message: '확인했습니다 :)',
+    imageUrl: 'path/to/image1.jpg',
+  },
+  {
+    id: 2,
+    name: '최수빈',
+    message: '감사합니다!! 조심히 가세요!',
+    imageUrl: 'path/to/image2.jpg',
+  },
+  {
+    id: 3,
+    name: '오수아',
+    message: '네넵 수고하세요',
+    imageUrl: 'path/to/image3.jpg',
+  },
+  {
+    id: 4,
+    name: '신도윤',
+    message: '혹시 판매됐나요?',
+    imageUrl: 'path/to/image4.jpg',
+  },
+  {
+    id: 5,
+    name: '김서윤',
+    message: '잘쓰세요~~',
+    imageUrl: 'path/to/image5.jpg',
+  },
+  {
+    id: 6,
+    name: '신서윤',
+    message: '후면 사진 한번 볼 수 있을까요?',
+    imageUrl: 'path/to/image6.jpg',
+  },
+  {
+    id: 7,
+    name: '김서윤',
+    message: '내일 거래 가능하세요?',
+    imageUrl: 'path/to/image7.jpg',
+  },
+  {
+    id: 8,
+    name: '이수아',
+    message: '이번 주 금요일에 뵙겠습니다!',
+    imageUrl: 'path/to/image8.jpg',
+  },
+  {
+    id: 9,
+    name: '최하은',
+    message: '제품 상태좀 볼 수 있을까요?',
+    imageUrl: 'path/to/image9.jpg',
+  },
+];
 
-    return (
-        <div>
-            <h1>Chat Room List</h1>
-            <p>Chat Room List Page</p>
-        </div>
-    )
-}
+const ChatRoomListPage = () => {
+  return (
+    <div className="chat-list">
+      <h2 className="chat-list-title">채팅</h2>
+      <ul className="chat-list-items">
+        {chats.map(chat => (
+          <li key={chat.id} className="chat-item">
+            <div className="chat-item-content">
+              <p className="chat-item-name">{chat.name}</p>
+              <p className="chat-item-message">{chat.message}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ChatRoomListPage;
