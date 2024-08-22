84103cb56f7088eccf39c2a216df5082570b8db5
Austin-Choi <austinsupra@gmail.com>
2024-08-22 14:50:06 +0900

    refactoring : 내 보안 탭 인증 필요없는 axios에 env 도메인 적용

--- src/pages/MyPage/components/MySecurity.jsx
+++ src/pages/MyPage/components/MySecurity.jsx
@@ -15,18 +15,14 @@ function MySecurity() {
     const token = localStorage.getItem('Authorization');
 
     try {
-      await axios.post(
-        '/api/auth/signout',
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json',
-          },
-
-          withCredentials: true,
-        }
-      );
+      // '/api/auth.signout'
+      await axios.post('auth/signout', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        baseURL: import.meta.env.VITE_BASE_URL,
+        withCredentials: true,
+      });
       // 로그아웃 후 로컬 스토리지에서 토큰 제거
       localStorage.removeItem('Authorization');
       console.log('User logged out');
@@ -44,11 +40,14 @@ function MySecurity() {
       const token = localStorage.getItem('Authorization');
 
       try {
-        await axios.delete('/api/members', {
+        // '/api/members'
+        await axios.delete('members', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
+          baseURL: import.meta.env.VITE_BASE_URL,
         });
+
         console.log('User account deleted');
         await handleLogout();
       } catch (error) {
