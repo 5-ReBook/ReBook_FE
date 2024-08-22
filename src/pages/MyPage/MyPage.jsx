36e52db9fb280decbeb2c18960f360a5a182fcfd
Austin-Choi <austinsupra@gmail.com>
2024-08-22 14:21:01 +0900

    refactoring : MyPage 공용 레이아웃 적용

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,31 +1,45 @@
-import React, { useState } from 'react';
-import MembersLayout from '../../components/Layouts/MembersLayout';
+import React, { useState, useEffect } from 'react';
 import SlidingSwitch from './components/SlidingSwitch';
-import './styles/MyPage.css';
 import MyInfo from './components/MyInfo';
 import MySecurity from './components/MySecurity';
+import {
+  defaultLayoutConfig,
+  useLayout,
+} from '../../components/Layouts/provider/LayoutProvider';
 
 function MyPage() {
   const [activeTab, setActiveTab] = useState('info');
+  const { setLayoutConfig } = useLayout();
+  useEffect(() => {
+    import('./styles/MyPage.css');
+    setLayoutConfig({
+      header: true,
+      leftButton: 'goBack',
+      footerNav: false,
+    });
+
+    // 컴포넌트가 언마운트될 때 레이아웃을 기본값으로 복원
+    return () => {
+      setLayoutConfig(defaultLayoutConfig);
+    };
+  }, [setLayoutConfig, defaultLayoutConfig]);
 
   return (
-    <MembersLayout>
-      <div className="mypage-outer-container">
-        <SlidingSwitch activeTab={activeTab} setActiveTab={setActiveTab} />
-        <div
-          className="myinfo-container__content"
-          style={{ display: activeTab === 'info' ? 'block' : 'none' }}
-        >
-          <MyInfo />
-        </div>
-        <div
-          className="mysecurity-container__content"
-          style={{ display: activeTab === 'security' ? 'block' : 'none' }}
-        >
-          <MySecurity />
-        </div>
+    <div className="mypage-outer-container">
+      <SlidingSwitch activeTab={activeTab} setActiveTab={setActiveTab} />
+      <div
+        className="myinfo-container__content"
+        style={{ display: activeTab === 'info' ? 'block' : 'none' }}
+      >
+        <MyInfo />
       </div>
-    </MembersLayout>
+      <div
+        className="mysecurity-container__content"
+        style={{ display: activeTab === 'security' ? 'block' : 'none' }}
+      >
+        <MySecurity />
+      </div>
+    </div>
   );
 }
 
