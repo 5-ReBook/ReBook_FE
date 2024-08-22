a24f7fde1264262547f6a4fa7badd811d7814b95
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-08-22 22:45:23 +0900

    [feat] SellerInfo 디자인 설정

--- src/components/Product/SellerInfo.jsx
+++ src/components/Product/SellerInfo.jsx
@@ -1,17 +1,29 @@
+import ProfileImage from '../Common/ProfileImage';
+import './SellerInfo.css';
+
 const SellerInfo = ({
   sellerImageUrl,
   sellerName,
   sellerUniversity,
+  sellerMajor,
   status,
 }) => {
   return (
     <div className="seller-info">
-      <img src={sellerImageUrl} alt={sellerName} />
+      <ProfileImage
+        src={`https://rb-dev-s3-images.s3.amazonaws.com/product/${sellerImageUrl}`}
+        name={`${sellerName} 의 프로필사진`}
+        size="md"
+      />
       <div>
         <h3>{sellerName}</h3>
-        <p>{sellerUniversity}</p>
+        <p>
+          {sellerUniversity} {sellerMajor}
+        </p>
       </div>
-      <span className="status">{status}</span>
+      <span className={`status-label ${status.toLowerCase()}`}>
+        {status === 'PENDING' ? '판매중' : '판매완료'}
+      </span>
     </div>
   );
 };
