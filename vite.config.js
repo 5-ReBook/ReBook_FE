5792897aaf3f1b17c49e2710c1a191a1d7858e77
Austin-Choi <austinsupra@gmail.com>
2024-08-22 19:27:00 +0900

    chore : build 옵션을 추가해 로딩 시간과 번들링 시간을 줄임

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,8 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    target: 'esnext', // 최신 브라우저를 대상으로 빌드하여 빠른 로딩 시간 확보
+    minify: 'esbuild', // 기본적으로 esbuild를 사용하여 빠른 번들링 및 최소화
+  },
 });
