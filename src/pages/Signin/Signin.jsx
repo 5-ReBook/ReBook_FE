3f35c3ea79750b53a1d36e1fbeb1785006e13caf
Austin-Choi <austinsupra@gmail.com>
2024-08-27 22:27:11 +0900

    patch : 띄어쓰기 수정

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -105,7 +105,7 @@ function Signin() {
       </button>
       <p className="forgot-password-text">
         비밀번호를 혹시 잊어버렸습니까?
-        <br /> 그렇다면
+        <br /> 그렇다면&nbsp;
         <Link to="/findpassword" state={{ username }}>
           여기
         </Link>
