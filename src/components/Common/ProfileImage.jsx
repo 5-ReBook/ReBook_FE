7e63e5a067d48709fb934291d296236f1a32ea2c
Austin-Choi <austinsupra@gmail.com>
2024-09-03 08:48:46 +0900

    patch : 기본 프로필 이미지 지정되지 않은 부분 수정

--- src/components/Common/ProfileImage.jsx
+++ src/components/Common/ProfileImage.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 import { Avatar } from '@chakra-ui/react';
 
-const ProfileImage = ({ src, name = "XprofileImage", size = "xl", fill }) => {
+function ProfileImage({ src, name = "XprofileImage", size = "xl", fill }) {
   return (
     <Avatar
       size={size}
       name={name}
       src={src}
       onError={(e) => {
-        e.currentTarget.src = defaultSrc || 'https://via.placeholder.com/150';
+        e.currentTarget.src = 'src/assets/images/basicprofile.png' || 'https://via.placeholder.com/150';
       }}
     />
   );
-};
+}
 
 export default ProfileImage;
