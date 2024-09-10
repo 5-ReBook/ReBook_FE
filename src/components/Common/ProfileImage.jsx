1efc78aeae633f931db11e45cc56ea20a8933529
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-09-10 09:24:39 +0900

    [chore]

--- src/components/Common/ProfileImage.jsx
+++ src/components/Common/ProfileImage.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { Avatar } from '@chakra-ui/react';
 
-function ProfileImage({ src, name = "XprofileImage", size = "xl", fill }) {
+function ProfileImage({ src, name = 'XprofileImage', size = 'xl', fill }) {
   return (
     <Avatar
       size={size}
       name={name}
       src={src}
-      onError={(e) => {
-        e.currentTarget.src = 'src/assets/images/basicprofile.png' || 'https://via.placeholder.com/150';
+      onError={e => {
+        e.currentTarget.src = defaultSrc || 'https://via.placeholder.com/150';
       }}
     />
   );
